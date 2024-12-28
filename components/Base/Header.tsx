fa19aab6fc4534adddb54b781d31e3e16f8f551a
tatsuya-16 <ta16tech@gmail.com>
2024-12-28 14:42:09 +0900

    add:header

---
+++ components/Base/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+    return (
+        <header className="p-4 bg-gray-800 text-white">
+            <nav className="flex justify-around">
+                <Link href="/" passHref>
+                    <span className="text-white no-underline cursor-pointer">Home</span>
+                </Link>
+                <Link href="/about" passHref>
+                    <span className="text-white no-underline cursor-pointer">About</span>
+                </Link>
+                <Link href="/blog" passHref>
+                    <span className="text-white no-underline cursor-pointer">Blog</span>
+                </Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;

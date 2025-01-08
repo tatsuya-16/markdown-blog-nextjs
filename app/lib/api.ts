ef8f48e7a3c2125d058f1b4f1dd1db68e77cb24a
tatsuya-16 <ta16tech@gmail.com>
2025-01-08 17:15:36 +0900

    fix:small fix

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -4,7 +4,10 @@ import matter from "gray-matter";
 import { join } from "path";
 
 // const postsDirectory = join(process.cwd(), "test");
-const postsDirectory = process.cwd() + '/test'; // 現在の作業ディレクトリを基準に設定
+// const postsDirectory = process.cwd() + '/test'; // 現在の作業ディレクトリを基準に設定
+const postsDirectory = process.env.NEXT_PUBLIC_API_URL
+  ? join(process.env.NEXT_PUBLIC_API_URL, "test")
+  : process.cwd() + '/test';
 
 
 export function getPostSlugs() {

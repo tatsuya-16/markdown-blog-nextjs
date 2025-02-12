403bfcc90bda63293cd6866c056bc094067e8051
tatsuya-16 <ta16tech@gmail.com>
2025-02-12 21:19:31 +0900

    add: private component

---
+++ app/private/20250214/page.tsx
@@ -0,0 +1,9 @@
+import { ValentinesDay2025 } from '@/components/private/ValentinesDay2025'
+
+const page20250214 = () => {
+    return (
+        <ValentinesDay2025/>
+    )
+}
+
+export default page20250214

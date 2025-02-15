ae91fa68504fa9ffd48e8b46e9412711a89476bb
tatsuya-16 <ta16tech@gmail.com>
2025-02-15 17:44:53 +0900

    remove: app/private

--- app/private/20250214/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ValentinesDay2025 } from '@/components/private/ValentinesDay2025'
-
-const page20250214 = () => {
-    return (
-        <ValentinesDay2025/>
-    )
-}
-
-export default page20250214

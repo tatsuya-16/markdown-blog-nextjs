85b4c921f61eb429aa947e10c2ce7d439d24be91
tatsuya-16 <ta16tech@gmail.com>
2025-10-09 18:51:35 +0900

    fix: fide some sections and link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Skills from "../components/Base/home/Skills"
+// import Skills from "../components/Base/home/Skills"
 import About from "@/components/Base/home/About"
-import RecentBlogs from "@/components/Base/home/Blogs"
-import TravelPhoto from "@/components/Base/home/TravelPhoto"
-import { headers } from 'next/headers';
-import { Photo } from './interfaces/photo';
+// import RecentBlogs from "@/components/Base/home/Blogs"
+// import TravelPhoto from "@/components/Base/home/TravelPhoto"
+// import { headers } from 'next/headers';
+// import { Photo } from './interfaces/photo';
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -11,35 +11,35 @@ export const metadata: Metadata = {
   description: "Tatsuya Abe's personal website and blog. (阿部竜弥の個人サイトおよびブログです。)",
 };
 
-async function fetchDomesticPhotos(host: string) {
-  const response = await fetch(`${process.env.API_PREFIX}${host}/api/travelPhotosDometic`, {
-      cache: "no-cache",
-  });
+// async function fetchDomesticPhotos(host: string) {
+//   const response = await fetch(`${process.env.API_PREFIX}${host}/api/travelPhotosDometic`, {
+//       cache: "no-cache",
+//   });
 
-  return response.json();
-}
+//   return response.json();
+// }
 
-async function fetchOverseasPhotos(host: string) {
-  const response = await fetch(`${process.env.API_PREFIX}${host}/api/travelPhotosOverseas`, {
-      cache: "no-cache",
-  });
+// async function fetchOverseasPhotos(host: string) {
+//   const response = await fetch(`${process.env.API_PREFIX}${host}/api/travelPhotosOverseas`, {
+//       cache: "no-cache",
+//   });
 
-  return response.json();
-}
+//   return response.json();
+// }
 
 export default async function Home() {
-  const host = (await headers()).get('host');
-  const domesticPhotos: Photo[] = await fetchDomesticPhotos(host!);
-  const overseasPhotos: Photo[] = await fetchOverseasPhotos(host!);
+  // const host = (await headers()).get('host');
+  // const domesticPhotos: Photo[] = await fetchDomesticPhotos(host!);
+  // const overseasPhotos: Photo[] = await fetchOverseasPhotos(host!);
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-300">
       <main className="flex-1">
         <About />
-        <Skills />
-        <RecentBlogs />
-        <TravelPhoto photoData={overseasPhotos} title="Overseas"/>
-        <TravelPhoto photoData={domesticPhotos} title="Domestic"/>
+        {/* <Skills /> */}
+        {/* <RecentBlogs /> */}
+        {/* <TravelPhoto photoData={overseasPhotos} title="Overseas"/> */}
+        {/* <TravelPhoto photoData={domesticPhotos} title="Domestic"/> */}
       </main>
     </div>
   );
---
+++ public/blogs/nuxt_flask.md
@@ -0,0 +1,10 @@
+---
+title: 
+slug: nuxt_flask
+tags:
+  - 
+author: Tatsuya Abe
+abstract: 
+date: '2025/7/2'
+image: https://fsbezimxrqnvxjyhivvn.supabase.co/storage/v1/object/public/blogThumbnail//flutter.svg
+---

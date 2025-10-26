22a680e58162e5a217f8e907ce44b69cde771896
tatsuya-16 <ta16tech@gmail.com>
2025-10-26 18:41:51 +0900

    fix auto deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 cd "$(dirname "$0")"
 
+echo "Deploying markdown-blog-nextjs..."
 git pull orgin main
 
+echo "Installing dependencies..."
 npm install
 
+echo "Building the project..."
 rpm run build
 
+echo "Restarting the application with PM2..."
 pm2 restart markdown-blog-nextjs

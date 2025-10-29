0e95825bab6fd51d22816f56a0b99a0cb01c58cd
tatsuya-16 <ta16tech@gmail.com>
2025-10-29 23:40:11 +0900

    fix blog top page to show only list style

--- components/Base/blog/AllBlogs.tsx
+++ components/Base/blog/AllBlogs.tsx
@@ -1,39 +1,45 @@
 'use client';
 
-import { useEffect, useState } from "react";
-import BlogCard from "./BlogCard";
+// import { useEffect, useState } from "react";
+// import BlogCard from "./BlogCard";
 import BlogList from "./BlogList";
 import { Blog } from "../../../app/interfaces/blog";
-import { Switch } from "../../ui/switch";
-import { Label } from "../../ui/label";
+// import { Switch } from "../../ui/switch";
+// import { Label } from "../../ui/label";
 
 export default function BlogsClient({ allPosts }: { allPosts: Blog[] }) {
-    const [isListView, setIsListView] = useState<boolean>(false);
-    const [isMounted, setIsMounted] = useState(false); // 初回マウントをトラッキング
+    // const [isListView, setIsListView] = useState<boolean>(false);
+    // const [isMounted, setIsMounted] = useState(false); // 初回マウントをトラッキング
 
-    useEffect(() => {
-        // マウント時に localStorage の値を取得
-        const storedViewMode = localStorage.getItem("viewMode");
-        if (storedViewMode === "list") {
-            setIsListView(true);
-        }
-        setIsMounted(true); // マウント完了
-    }, []);
+    // useEffect(() => {
+    //     // マウント時に localStorage の値を取得
+    //     const storedViewMode = localStorage.getItem("viewMode");
+    //     if (storedViewMode === "list") {
+    //         setIsListView(true);
+    //     }
+    //     setIsMounted(true); // マウント完了
+    // }, []);
 
-    useEffect(() => {
-        if (isMounted) {
-            localStorage.setItem("viewMode", isListView ? "list" : "grid");
-        }
-    }, [isListView, isMounted]);
+    // useEffect(() => {
+    //     if (isMounted) {
+    //         localStorage.setItem("viewMode", isListView ? "list" : "grid");
+    //     }
+    // }, [isListView, isMounted]);
 
     return (
         <div>
-            <div className="flex items-center justify-center space-x-2 my-4">
+            {/* <div className="flex items-center justify-center space-x-2 my-4">
             <Label htmlFor="view-mode">Grid</Label>
             <Switch id="view-mode" checked={isListView} onCheckedChange={setIsListView}/>
             <Label htmlFor="view-mode">List</Label>
+            </div> */}
+            <div className="list-view">
+                {allPosts.map((blog) => (
+                <BlogList key={blog.slug} blogData={blog} />
+                ))}
             </div>
-            {isListView ? (
+            
+            {/* {isListView ? (
             <div className="list-view">
                 {allPosts.map((blog) => (
                 <BlogList key={blog.slug} blogData={blog} />
@@ -45,7 +51,7 @@ export default function BlogsClient({ allPosts }: { allPosts: Blog[] }) {
                 <BlogCard key={blog.slug} blogData={blog} />
                 ))}
             </div>
-            )}
+            )} */}
         </div>
     );
 }
